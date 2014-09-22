8f2bd4ecdd18da1ab16a82131dc2ecc461c641e7
fbuecklers <fb@baqend.com>
2014-09-22 10:32:48 +0200

    new iframe connector

--- Gruntfile.js
+++ Gruntfile.js
@@ -145,7 +145,7 @@ module.exports = function (grunt) {
       test: {
         hostname: 'jenkins.baqend.com',
         configFile: 'karma.conf.js',
-        browsers: ['PhantomJS', 'Chrome', 'Firefox', 'IE9', 'Chrome-win'],
+        browsers: ['PhantomJS', 'Chrome', 'Firefox', 'IE9-Win', 'IE10-Win'],
         reporters: ['junit'],
         singleRun: true,
         junitReporter: {
