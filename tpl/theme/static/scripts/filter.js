e93a34970735b3520a391c8bab1940c551abbdfe
fbuecklers <fb@baqend.com>
2015-03-30 15:55:17 +0200

    new layout for jsdoc and js guide

--- tpl/theme/static/scripts/filter.js
+++ tpl/theme/static/scripts/filter.js
@@ -1,23 +1,23 @@
 (function() {
-  var nav = document.querySelector('nav');
+  var nav = document.querySelector('.bs-sidebar');
   var filter = nav.querySelector('.filter');
-  var openables = nav.querySelectorAll('ul > li:not(.opened)');
+  var openables = nav.querySelectorAll('ul > li:not(.active)');
 
   filter.addEventListener('keyup', function() {
     var str = filter.value.toLowerCase();
 
     for (var i = 0, openable; openable = openables[i]; i++) {
       if (str) {
-        openable.classList.add('opened');
+        openable.classList.add('active');
       } else {
-        openable.classList.remove('opened');
+        openable.classList.remove('active');
       }
     }
 
     var lis = nav.querySelectorAll('li');
     loop: for (var i = 0, li; li = lis[i]; i++) {
       if (str) {
-        var names = li.querySelectorAll('.name');
+        var names = li.querySelectorAll('.nav-name');
         for (var j = 0, name; name = names[j]; j++) {
           if (name.innerText.toLowerCase().indexOf(str) != -1) {
             li.style.display = 'block';
