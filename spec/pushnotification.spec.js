49d1ca531d05680905d0935a940a02cb94aa6d2d
Hannes <hk@baqend.com>
2015-08-26 17:38:32 +0200

    fixed pushnotification js test

--- spec/pushnotification.spec.js
+++ spec/pushnotification.spec.js
@@ -11,12 +11,19 @@ if (typeof DB == 'undefined') {
 describe('Test Push Notifications', function() {
   var emf, db;
 
-  var TEST_APNS_DEVICE = "ccc001e53aad739f3b155fbb7c277934604c850d013b509c3d1382fe2c84608b";
   var TEST_GCM_DEVICE = "APA91bFBRJGMI2OkQxhV3peP4ncZOIxGJBJ8s0tkKyWvzQErpZmuSzMzm6ugz3rOauMQ1CRui0bBsEQvuN0W8X1wTP547C6MSNcErnNYXyvc1F5eKZCs-GAtE_NcESolea2AM6_cRe9R";
+  var TEST_GCM_APIKEY = "AIzaSyCjswZVNArWwGT8Pb3ZyVYILyWJwzKWUwo";
 
   before(function() {
     emf = new DB.EntityManagerFactory(env.TEST_SERVER);
-    return emf.metamodel.init();
+
+    return emf.metamodel.init().then(function() {
+      db = emf.createEntityManager();
+      return db.login("root", "root");
+    }).then(function() {
+      var msg = new DB.message.GCMAKey(TEST_GCM_APIKEY);
+      return db._send(msg);
+    });
   });
 
   beforeEach(function() {
@@ -24,11 +31,11 @@ describe('Test Push Notifications', function() {
   });
 
   it('should register device', function() {
-    return Promise.all([db.Device.register("IOS", TEST_APNS_DEVICE), db.Device.register("Android", TEST_GCM_DEVICE)]);
+    return db.Device.register("Android", TEST_GCM_DEVICE);
   });
 
   it('should save registration in cookie', function() {
-    return db.Device.register("IOS", TEST_APNS_DEVICE).then(function() {
+    return db.Device.register("Android", TEST_GCM_DEVICE).then(function() {
       return emf.createEntityManager(true).ready();
     }).then(function(newDB) {
       expect(newDB.isDeviceRegistered).be.true;
@@ -38,13 +45,12 @@ describe('Test Push Notifications', function() {
 
   it('should push message', function() {
     return db.login('root', 'root').then(function() {
-      return Promise.all([db.Device.register("IOS", TEST_APNS_DEVICE), db.Device.register("Android", TEST_GCM_DEVICE)]);
+      return db.Device.register("Android", TEST_GCM_DEVICE);
     }).then(function() {
-      return db.Device.find().in("deviceOs", ["Android", "IOS"]).resultList();
+      return db.Device.find().equal("deviceOs", "Android").resultList();
     }).then(function(result) {
-      expect(result).length.at.least(2);
-      var msg = new db.Device.PushMessage(result[0], "Message", "Subject");
-      msg.addDevice(result[1]);
+      expect(result).length.at.least(1);
+      var msg = new db.Device.PushMessage(result, "Message", "Subject");
       msg.sound = "default";
       msg.badge = 5;
       msg.data = {
@@ -56,13 +62,12 @@ describe('Test Push Notifications', function() {
 
   it('should create correct json from push message', function() {
     return db.login('root', 'root').then(function() {
-      return Promise.all([db.Device.register("IOS", TEST_APNS_DEVICE), db.Device.register("Android", TEST_GCM_DEVICE)]);
+      return db.Device.register("Android", TEST_GCM_DEVICE);
     }).then(function() {
-      return db.Device.find().in("deviceOs", ["Android", "IOS"]).resultList();
+      return db.Device.find().equal("deviceOs", "Android").resultList();
     }).then(function(result) {
       var msg1 = new db.Device.PushMessage();
       msg1.addDevice(result[0]);
-      msg1.addDevice(result[1]);
       msg1.message = "TestMSG";
       msg1.subject = "TestSubject";
       msg1.badge = 5;
@@ -70,7 +75,7 @@ describe('Test Push Notifications', function() {
         "test": "test"
       };
       msg1.sound = "test";
-      var msg2 = new db.Device.PushMessage(result, "TestMSG", "TestSubject", "test", 5, { test: "test" });
+      var msg2 = new db.Device.PushMessage(result[0], "TestMSG", "TestSubject", "test", 5, { test: "test" });
       expect(msg2.toJSON()).eql(msg1.toJSON());
     });
   });
