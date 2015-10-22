d16a6c11f070d6480108765bae5e4fe665d7d4dc
fbuecklers <fb@baqend.com>
2015-10-22 13:06:04 +0200

    fix date creation

--- spec/types.spec.js
+++ spec/types.spec.js
@@ -168,7 +168,7 @@ describe('Test entity type', function () {
   });
 
   describe("date value", function() {
-    test("date", new Date(0), new Date("2013-11-22Z"), new Date("2013-11-22Z"), new Date("0000-01-01Z"));
+    test("date", new Date(0), new Date("2013-11-22T00:00Z"), new Date("2013-11-22T00:00Z"), new Date("0000-01-01T00:00Z"));
   });
 
   describe("dateTime value", function() {
