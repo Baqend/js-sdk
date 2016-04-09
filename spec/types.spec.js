ad80bd55140c7a9c46a26a390efe2a5a6c6e2776
fb <fb@baqend.com>
2016-04-10 00:32:55 +0200

    implement new collection tracking and handle dates correctly

--- spec/types.spec.js
+++ spec/types.spec.js
@@ -22,7 +22,7 @@ describe('Test entity type', function () {
     "geoPoint": { type: metamodel.baseType('GeoPoint'), values: [new DB.GeoPoint(34.5658, 110.4576), new DB.GeoPoint(0, 0)] },
     "jsonArray": { type: metamodel.baseType('JsonArray'), values: [[1,'string',{key:'value'}, [1,2,3]], [], [null, 0, false, '', [], {}]] },
     "jsonObject": { type: metamodel.baseType('JsonObject'), values: [
-      {k1:1, k2:'string', k3:{key:'value'}, k4:[1,2,3]}, {}, {k1:null, k2:0, k3:false, k4:'', k5:[], k6:{}}
+      {k1:1, k2:'string', k3:{key:'value'}, k4:[1,2,3]}, {}, {k1:3.4, k2:0, k3:false, k4:'', k5:[], k6:{}}
     ]}
   };
 
@@ -98,6 +98,10 @@ describe('Test entity type', function () {
   }
 
   function setValues(obj) {
+    for (var prop in obj)
+      if (prop != 'id')
+        obj[prop] = null;
+
     obj.boolean = true;
     obj.float = 1.1;
     obj.integer = 45;
@@ -174,16 +178,16 @@ describe('Test entity type', function () {
         list.push(vals[i]);
       }
 
-      test(name + "List", [list, list]);
+      testList(name + "List", list);
     });
   });
 
   describe("refList value", function() {
-    test("refList", [new DB.List()], [new DB.List(testObject(), testObject(), null, testObject())]);
+    testList("refList", new DB.List(testObject(), testObject(), null, testObject()));
   });
 
   describe("embeddedList value", function() {
-    test("embeddedList", [new DB.List()], [new DB.List(embeddableObject(), embeddableObject(), null, embeddableObject())]);
+    testList("embeddedList", new DB.List(embeddableObject(), embeddableObject(), null, embeddableObject()));
   });
 
   for (name in types) {
@@ -195,13 +199,13 @@ describe('Test entity type', function () {
           set.add(vals[i]);
         }
 
-        test(name + "Set", set);
+        testSet(name + "Set", set);
       });
     }
   }
 
   describe("refSet value", function() {
-    test("refSet", new DB.Set(), new DB.Set([testObject(), null, testObject(), testObject()]));
+    testSet("refSet", new DB.Set([testObject(), null, testObject(), testObject()]));
   });
 
   describe("simpleMap value", function() {
@@ -210,35 +214,7 @@ describe('Test entity type', function () {
       ["String", false]
     ]);
 
-    test("simplebooleanMap", new DB.Map(), new DB.Map(map));
-
-    it("should add key", function() {
-      obj.simplebooleanMap = new DB.Map(map);
-      obj.simplebooleanMap.set("123", null);
-
-      var json = state.getJson();
-      json.simplebooleanMap['add'] = true;
-      state.setJson(json);
-
-      expect(obj.simplebooleanMap.size).equals(4);
-      expect(obj.simplebooleanMap.get('123')).be.null;
-      expect(obj.simplebooleanMap.get('add')).be.true;
-      expect(obj.simplebooleanMap.get('Test')).be.true;
-      expect(obj.simplebooleanMap.get('String')).be.false;
-    });
-
-    it("should remove key", function() {
-      obj.simplebooleanMap = new DB.Map(map);
-
-      var json = state.getJson();
-      delete json.simplebooleanMap['123'];
-      state.setJson(json);
-
-      expect(obj.simplebooleanMap.size).equals(2);
-      expect(obj.simplebooleanMap.get('123')).be.undefined;
-      expect(obj.simplebooleanMap.get('Test')).be.true;
-      expect(obj.simplebooleanMap.get('String')).be.false;
-    });
+    testMap("simplebooleanMap", map);
   });
 
   Object.keys(types).forEach(function(name) {
@@ -249,7 +225,7 @@ describe('Test entity type', function () {
         map.set("key" + i, vals[i]);
       }
 
-      test("simple" + name + "Map", map);
+      testMap("simple" + name + "Map", map);
     });
 
     if (types[name].simple) {
@@ -260,7 +236,7 @@ describe('Test entity type', function () {
           map.set(keys[i], "value" + i);
         }
 
-        test(name + "simpleMap", map);
+        testMap(name + "simpleMap", map);
       });
     }
   });
@@ -273,7 +249,7 @@ describe('Test entity type', function () {
     ]);
 
     //null values should be removed
-    test("simpleRefMap", new DB.Map(), map1);
+    testMap("simpleRefMap", map1);
 
     it("should remove null values", function() {
       var map2 = new DB.Map(map1);
@@ -293,7 +269,7 @@ describe('Test entity type', function () {
       [testObject(), "value2"]
     ]);
 
-    test("refSimpleMap", new DB.Map(), map1);
+    testMap("refSimpleMap", map1);
 
     it("should remove null values", function() {
       var nullKey = testObject();
@@ -314,7 +290,7 @@ describe('Test entity type', function () {
       [testObject(), testObject()]
     ]);
 
-    test("refMap", new DB.Map(), map1);
+    testMap("refMap", map1);
 
     it("should remove null values", function() {
       var nullKey = testObject();
@@ -335,7 +311,7 @@ describe('Test entity type', function () {
       ["String", embeddableObject()]
     ]);
 
-    test("simpleEmbeddedMap", new DB.Map(), map1);
+    testMap("simpleEmbeddedMap", map1);
 
     it("should keep embedded identity", function() {
       obj["simpleEmbeddedMap"] = new DB.Map(map1);
@@ -365,7 +341,7 @@ describe('Test entity type', function () {
       [new Date(), embeddableObject()]
     ]);
 
-    test("dateTimeEmbeddedMap", new DB.Map(), map1);
+    testMap("dateTimeEmbeddedMap", map1);
 
     it("should keep embedded identity", function() {
       obj["dateTimeEmbeddedMap"] = new DB.Map(map1);
@@ -420,15 +396,6 @@ describe('Test entity type', function () {
         expectedValue = arg[1];
       } else {
         value = arg;
-      }
-
-      if (value instanceof DB.List) {
-        expectedValue = new DB.List.from(value);
-      } else if (value instanceof DB.Set) {
-        expectedValue = new DB.Set(value);
-      } else if (value instanceof DB.Map) {
-        expectedValue = new DB.Map(value);
-      } else {
         expectedValue = value;
       }
 
@@ -436,22 +403,180 @@ describe('Test entity type', function () {
         obj[name] = value;
 
         return obj.save({refresh:true}).then(function() {
-          if (expectedValue instanceof DB.Map) {
-            var map = obj[name];
-            expect(map.size).eql(expectedValue.size);
-            for (var iter = expectedValue.entries(), item; !(item = iter.next()).done; ) {
-              expect(map.get(item.value[0])).eqls(item.value[1]);
-            }
-          } else {
-            expect(obj[name]).eql(expectedValue);
+          expect(obj[name]).eql(expectedValue);
+        });
+      });
+    });
+  }
+
+  function testList(name, list) {
+    test(name);
+
+    it(name + " should be saved with an empty list", function() {
+      obj[name] = new DB.List();
+
+      return obj.save({refresh: true}).then(function() {
+        var list = obj[name];
+        expect(list.length).eql(0);
+      });
+    });
+
+    it(name + " should add values", function() {
+      obj[name] = DB.List.from(list);
+
+      return obj.save().then(function() {
+        obj[name].splice(0, obj[name].length);
+        expect(obj[name].length).eql(0);
+        return obj.load({refresh: true});
+      }).then(function() {
+        var newList = obj[name];
+        expect(newList.length).eql(list.length);
+        for (var i = 0; i < list.length; ++i) {
+          expect(newList[i]).eql(list[i]);
+        }
+      });
+    });
+
+    it(name + " should remove values", function() {
+      var newList = obj[name] = DB.List.from(list);
+
+      var json = state.getJson();
+      json[name] = [];
+      state.setJson(json);
+
+      expect(newList.length).eq(0);
+    });
+
+    var args = Array.prototype.slice.call(arguments, 1);
+    args.forEach(function(arg) {
+      var value = arg;
+      var expectedValue = DB.List.from(value);
+
+      it(name + " should be saved and reloaded", function() {
+        obj[name] = value;
+
+        return obj.save({refresh: true}).then(function() {
+          var list = obj[name];
+          var len = expectedValue.length;
+          expect(list.length).eql(len);
+          for (var i = 0; i < len; ++i) {
+            expect(list[i]).eql(expectedValue[i]);
           }
         });
       });
     });
   }
 
-  function testList(test) {
-    test(test);
+  function testSet(name, set) {
+    test(name);
 
+    it(name + " should be saved with an empty set", function() {
+      obj[name] = new DB.Set();
+
+      return obj.save({refresh: true}).then(function() {
+        var set = obj[name];
+        expect(set.size).eql(0);
+      });
+    });
+
+    it(name + " should add values", function() {
+      obj[name] = new DB.Set(set);
+
+      return obj.save().then(function() {
+        obj[name].clear();
+        return obj.load({refresh: true});
+      }).then(function() {
+        var newSet = obj[name];
+        expect(newSet.size).gt(0);
+        for (var iter = set.values(), item; !(item = iter.next()).done;) {
+          expect(newSet.has(item.value)).be.true;
+        }
+      });
+    });
+
+    it(name + " should remove values", function() {
+      var newSet = obj[name] = new DB.Set(set);
+
+      var json = state.getJson();
+      json[name] = [];
+      state.setJson(json);
+
+      expect(newSet.size).eq(0);
+    });
+
+    var args = Array.prototype.slice.call(arguments, 1);
+    args.forEach(function(arg) {
+      var value = arg;
+      var expectedValue = new DB.Set(value);
+
+      it(name + " should be saved and reloaded", function() {
+        obj[name] = value;
+
+        return obj.save({refresh: true}).then(function() {
+          var set = obj[name];
+          var size = expectedValue.size;
+          expect(set.size).eql(size);
+          expectedValue.forEach(function(val) {
+            expect(set.has(val)).be.true;
+          });
+        });
+      });
+    });
+  }
+
+  function testMap(name, map) {
+    test(name);
+
+    it(name + " should be saved with an empty map", function() {
+      obj[name] = new DB.Map();
+
+      return obj.save({refresh: true}).then(function() {
+        var map = obj[name];
+        expect(map.size).eql(0);
+      });
+    });
+
+    it(name + " should add key", function() {
+      obj[name] = new DB.Map(map);
+
+      return obj.save().then(function() {
+        obj[name].clear();
+        return obj.load({refresh: true});
+      }).then(function() {
+        var newMap = obj[name];
+        expect(newMap.size).gt(0);
+        for (var iter = map.entries(), item; !(item = iter.next()).done;) {
+          expect(newMap.get(item.value[0])).eqls(item.value[1]);
+        }
+      });
+    });
+
+    it(name + " should remove key", function() {
+      var newMap = obj[name] = new DB.Map(map);
+
+      var json = state.getJson();
+      json[name] = {};
+      state.setJson(json);
+
+      expect(newMap.size).eq(0);
+    });
+
+    var args = Array.prototype.slice.call(arguments, 1);
+    args.forEach(function(arg) {
+      var value = arg;
+      var expectedValue = new DB.Map(value);
+
+      it(name + " should be saved and reloaded", function() {
+        obj[name] = value;
+
+        return obj.save({refresh: true}).then(function() {
+          var map = obj[name];
+          expect(map.size).eql(expectedValue.size);
+          for (var iter = expectedValue.entries(), item; !(item = iter.next()).done;) {
+            expect(map.get(item.value[0])).eqls(item.value[1]);
+          }
+        });
+      });
+    });
   }
 });
