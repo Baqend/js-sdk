7b6f44b4fb7695140ca300734da3fdf127c8f582
fb <fb@baqend.com>
2016-04-14 18:00:28 +0200

    fix baqend distribution for ie < edge

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,35 +16,33 @@ module.exports = function (grunt) {
   };
 
   function injectBabelHelper(b, opts) {
-    if (!opts.debug) {
-      //list of helpers babel-core\lib\transformation\file\index.js
-      var helper = require("babel-core").buildExternalHelpers([
-        'inherits',
-        'createClass',
-        'defaults',
-        'classCallCheck',
-        'possibleConstructorReturn',
-        'typeof'
-      ], 'var');
+    //list of helpers babel-core\lib\transformation\file\index.js
+    var helper = require("babel-core").buildExternalHelpers([
+      'inherits',
+      'createClass',
+      'defaults',
+      'classCallCheck',
+      'possibleConstructorReturn',
+      'typeof'
+    ], 'var');
 
-      //make babel helpers ie < 11 ready
-      helper = helper.replace('subClass\.__proto__ = superClass', '_defaults(subClass, superClass)');
+    //make babel helpers ie < 11 ready
+    helper = helper.replace('subClass\.__proto__ = superClass', 'babelHelpers.defaults(subClass, superClass)');
 
-      var first = true;
-      b.pipeline.get('pack').push(through.obj(
-          function (row, enc, next) {
-            if (first) {
-              var line = 'return ';
-              this.push(row.slice(0, row.length - line.length));
-              this.push(helper);
-              this.push(line);
-              first = false;
-            } else {
-              this.push(row)
-            }
-            next()
-          }));
-    }
+    var first = true;
+    b.pipeline.get('pack').push(through.obj(
+        function (row, enc, next) {
+          if (first) {
+            var line = 'return ';
+            this.push(row.slice(0, row.length - line.length));
+            this.push(helper);
+            this.push(line);
+            first = false;
+          } else {
+            this.push(row)
+          }
+          next()
+        }));
   }
 
   grunt.initConfig({
