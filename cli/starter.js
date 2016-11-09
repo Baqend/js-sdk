bb51759651a5e06090c19cd657c48229c4cf590b
fb <fb@baqend.com>
2016-11-09 12:07:07 +0100

    add missing dependencies for the cli

--- cli/starter.js
+++ cli/starter.js
@@ -9,28 +9,29 @@ const rimraf = require('rimraf');
 
 module.exports = function(name, dir) {
   if (!name) {
-    list();
+    return list();
   } else {
-    clone(name, dir);
+    return clone(name, dir);
   }
-
-  return true;
 };
+
+module.exports.clone = clone;
+module.exports.list = list;
 
 function clone(name, dir) {
   let folder = path.join(dir || name);
 
-  callGithub(`/repos/baqend/${name}`, (json) => {
+  return callGithub(`/repos/baqend/${name}`).then((json) => {
     if (json.name !== name) {
       console.log(`${name} not found.`);
     } else {
-      installStarter(name, folder);
+      return installStarter(name, folder);
     }
   });
 }
 
 function list() {
-  callGithub('/orgs/baqend/repos?type=public', (json) => {
+  return callGithub('/orgs/baqend/repos?type=public').then((json) => {
     console.log('Available starters');
     json.forEach(repo => {
       if (repo.name.endsWith('starter')) {
@@ -43,7 +44,7 @@ function list() {
 function installStarter(name, folder) {
   console.log(`cloning starter into ${folder}`);
 
-  simpleGit()
+  return simpleGit()
       .clone(`https://github.com/baqend/${name}`, folder)
       .then(() => {
         rimraf(path.join(folder, '.git'), () => {});
@@ -51,36 +52,41 @@ function installStarter(name, folder) {
         process.stdout.write('npm is installing...');
         let interval = setInterval(() => process.stdout.write('.'), 4000);
 
-        let child = exec('npm install', { cwd: folder });
-        child.stderr.pipe(process.stderr);
-        child.stdout.pipe(process.stdout);
-
-        child.on('exit', () => {
+        return new Promise((success, error) => {
+          let child = exec('npm install', { cwd: folder });
+          child.stderr.pipe(process.stderr);
+          child.stdout.pipe(process.stdout);
+          child.on('error', error);
+          child.on('exit', success);
+        }).then(() => {
           clearInterval(interval);
           console.log(`${name} has successfully been installed`);
         });
       });
 }
 
-function callGithub(url, cb) {
-  https.request({
-    host: 'api.github.com',
-    port: '443',
-    path: url,
-    method: 'GET',
-    headers: {
-      accept: 'application/vnd.github.v3+json',
-      'user-agent': 'baqend'
-    }
-  }, resp => {
-    let data = '';
+function callGithub(url) {
+  return new Promise((success, error) => {
+    https.request({
+      host: 'api.github.com',
+      port: '443',
+      path: url,
+      method: 'GET',
+      headers: {
+        accept: 'application/vnd.github.v3+json',
+        'user-agent': 'baqend'
+      }
+    }, resp => {
+      let data = '';
 
-    resp.on('data', chunk => {
-      data += chunk;
-    });
+      resp.on('data', chunk => {
+        data += chunk;
+      });
 
-    resp.on('end', () => cb(JSON.parse(data)));
-  }).on('error', function(e) {
-    console.log('problem with github request: ' + e.message);
-  }).end();
+      resp.on('end', () => success(JSON.parse(data)));
+    }).on('error', (e) => {
+      console.log('problem with github request: ' + e.message);
+      error(e);
+    }).end();
+  });
 }
