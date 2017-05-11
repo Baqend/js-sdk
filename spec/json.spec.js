064aaaf6935221c233db8aa7ee086d19336ca738
Julian Tiemann <jutiemann@gmail.com>
2017-05-11 16:09:20 +0200

    add tests

--- spec/json.spec.js
+++ spec/json.spec.js
@@ -75,7 +75,7 @@ describe("Test toJSON and fromJSON", function() {
     });
   });
 
-  describe("depth 0", function() {
+  describe("default", function() {
 
     it('should convert to JSON', function() {
       var obj, json;
@@ -117,7 +117,7 @@ describe("Test toJSON and fromJSON", function() {
     });
   })
 
-  describe("depth 1", function() {
+  describe("nested", function() {
     var obj;
     beforeEach(function() {
       obj = new db.Level0();
@@ -125,7 +125,7 @@ describe("Test toJSON and fromJSON", function() {
       obj.level1 = new db.Level1({value: "level1"})
     });
 
-    it('should convert to JSON with depth 1', function() {
+    it('should convert to JSON with nested objects', function() {
       var json = obj.toJSON({depth: 1});
       expect(json).be.ok;
       expect(json.value).eqls("level0")
@@ -133,7 +133,7 @@ describe("Test toJSON and fromJSON", function() {
       expect(json.level1.value).eqls("level1");
     });
 
-    it('should update existing object from JSON with depth 0', function() {
+    it('should update existing object from JSON without nested objects', function() {
       return obj.save({depth: 1}).then(function(obj) {
         expect(obj.value).eqls("level0");
         expect(obj.version).eqls(1);
@@ -152,7 +152,7 @@ describe("Test toJSON and fromJSON", function() {
       })
     })
 
-    it('should update existing object from JSON with depth 1', function() {
+    it('should update existing object from JSON with nested objects', function() {
       return obj.save({depth: 1}).then(function(obj) {
         expect(obj.value).eqls("level0");
         expect(obj.version).eqls(1);
@@ -169,6 +169,56 @@ describe("Test toJSON and fromJSON", function() {
         expect(obj.level1.value).eqls("level1 updated");
         expect(obj.level1.version).eqls(2);
         expect(obj.level1._metadata.isPersistent).eqls(true);
+      })
+    })
+
+  })
+
+  describe("List", function() {
+    var obj, listItem1, listItem2, listItem3;
+    beforeEach(function() {
+      listItem1 = new db.Level2({value: "listItem1"})
+      listItem2 = new db.Level2({value: "listItem2"})
+      listItem3 = new db.Level2({value: "listItem3"})
+      obj = new db.Level1({value: "level1"})
+      obj.testList = new DB.List(listItem1, listItem2)
+    });
+
+    it('should serialize a list with references', function() {
+      return obj.save({depth: 1}).then(function(obj) {
+        return Promise.all([db2.load(obj.testList[0].id), db2.load(obj.id)]);
+      }).then(function(loaded) {
+        var obj = loaded[1];
+        var json = obj.toJSON({depth: 1})
+        expect(json).be.ok
+        expect(typeof json.testList[0]).eqls('object')
+        expect(typeof json.testList[1]).eqls('string')
+        expect(json.testList[0].value).eqls('listItem1')
+      })
+    })
+
+    it('should create a list with references from json', function() {
+      var json = obj.toJSON({depth: 1})
+      obj = db.Level1.fromJSON(json)
+      return obj.save({depth: 1}).then(function(obj) {
+        return db2.Level1.load(obj.id, {depth: 1})
+      }).then(function(obj) {
+        expect(obj.testList[0]).to.have.property('id');
+        expect(obj.testList[1]).to.have.property('id');
+      })
+    })
+
+    it('should add a newly created object to a map from json', function() {
+      obj.save({depth: 1}).then(function(obj) {
+        var json = obj.toJSON({depth: 1})
+        json.testList.push(listItem3.toJSON());
+        obj = db.Level1.fromJSON(json)
+        return obj.save({depth: 1}).then(function(obj) {
+          return db2.Level1.load(obj.id, {depth: 1})
+        })
+      }).then(function(obj) {
+        expect(obj.testList.length).eqls(3);
+        expect(obj.testList[2]).to.have.property('id');
       })
     })
 
@@ -217,7 +267,88 @@ describe("Test toJSON and fromJSON", function() {
           return db2.Level1.load(obj.id, {depth: 1})
         })
       }).then(function(obj) {
-        expect(true).eqls(true)
+        expect(obj.testMap.size).eqls(3);
+        expect(obj.testMap.get('listItem3')).to.have.property('id');
+      })
+    })
+
+  })
+
+  describe("Set", function() {
+    var obj, listItem1, listItem2, listItem3;
+    beforeEach(function() {
+      listItem1 = new db.Level2({value: "listItem1"})
+      listItem2 = new db.Level2({value: "listItem2"})
+      listItem3 = new db.Level2({value: "listItem3"})
+      obj = new db.Level1({value: "level1"})
+      obj.testSet = new DB.Set([listItem1, listItem2])
+    });
+
+    it('should serialize a set with references', function() {
+      return obj.save({depth: 1}).then(function(obj) {
+        var it = obj.testSet.values()
+        var item = it.next().value
+        return Promise.all([db2.load(item.id), db2.load(obj.id)]);
+      }).then(function(loaded) {
+        var obj = loaded[1];
+        var json = obj.toJSON({depth: 1})
+        expect(json).be.ok
+        for (var i = 0; i < json.testSet.length; i++){
+          if(typeof json.testSet[i] === 'object')
+            expect(json.testSet[i].value).eqls('listItem1')
+        }
+      })
+    })
+
+    it('should create a map with references from json', function() {
+      var json = obj.toJSON({depth: 1})
+      obj = db.Level1.fromJSON(json)
+      return obj.save({depth: 1}).then(function(obj) {
+        return db2.Level1.load(obj.id, {depth: 1})
+      }).then(function(obj) {
+        var it = obj.testSet.values()
+        expect(it.next().value).to.have.property('id');
+        expect(it.next().value).to.have.property('id');
+      })
+    })
+
+    it('should add a newly created object to a map from json', function() {
+      obj.save({depth: 1}).then(function(obj) {
+        var json = obj.toJSON({depth: 1})
+        json.testSet.push(listItem3.toJSON());
+        obj = db.Level1.fromJSON(json)
+        return obj.save({depth: 1}).then(function(obj) {
+          return db2.Level1.load(obj.id, {depth: 1})
+        })
+      }).then(function(obj) {
+        expect(obj.testSet.size).eqls(3);
+        obj.testSet.forEach(function(item) {
+          if (item.value === 'listItem3')
+            expect(item).to.have.property('id');
+        })
+      })
+    })
+
+  })
+
+  describe("Embedded", function() {
+    var obj;
+    beforeEach(function() {
+      obj = new db.Level2({
+        value: "level2",
+        embedded: new db.Embedded({
+          value: 'embedded'
+        })
+      })
+    });
+
+    it('should serialize a object with embedded json', function() {
+      return obj.save().then(function(obj) {
+        return db2.load(obj.id)
+      }).then(function(obj) {
+        var json = obj.toJSON()
+        expect(json).be.ok
+        expect(json.embedded.value).eqls('embedded')
       })
     })
 
