02ac5ca53226b828dbd3b05f9b09101508b6e4ea
fb <fb@baqend.com>
2018-01-03 16:24:35 +0100

    Discuss final eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,35 +22,39 @@ module.exports = {
   rules: {
     // enforce consistent linebreak style
     // https://eslint.org/docs/rules/linebreak-style
-    'linebreak-style': 0,
+    'linebreak-style': 'off',
 
     strict: ['error', 'global'],
 
-    // allow _metadata for now, we might replace them bei Metadata.get calls
+    // FIXME allow all currently used _fields for now
     'no-underscore-dangle': ['error', { allow: ['_metadata'] }],
     // 'no-underscore-dangle': ['error', { allowAfterThis: true }],
 
     // Node 4 does not support backtick therefore disallow them
     quotes: ['error', 'single'],
+    'prefer-template': 'off',
+
     // comma dangling for functions is not supported in es2016
     'comma-dangle': ['error', {
       arrays: 'always-multiline',
       objects: 'always-multiline',
       imports: 'always-multiline',
       exports: 'always-multiline',
-      functions: 'ignore',
+      functions: 'never',
     }],
-    'prefer-template': 'off',
+
+    // Change these rules if we drop Node 4 support
     'prefer-spread': 'off',
     'prefer-rest-params': 'off',
     'prefer-destructuring': 'off',
-    'no-param-reassign': 'warn',
-    'class-methods-use-this': 'warn',
+    'no-param-reassign': 'error', // TODO: refactor all methods to self calls see example in UserFactory#login
+    'class-methods-use-this': 'off',
 
     // FIXME: Discuss, if we want to change new Buffer to Buffer.from(), since it is deprecated since node 6.
     // FIXME: But we must bump required node engine to at least 4.5 since Buffer.from() is not available beforehand
-    'node/no-deprecated-api': 'warn',
-    'no-buffer-constructor': 'warn',
+    // TODO: Lets do it!
+    'node/no-deprecated-api': 'error',
+    'no-buffer-constructor': 'error',
 
     // specify the maximum length of a line in your program
     // http://eslint.org/docs/rules/max-len
