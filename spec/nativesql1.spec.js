4464a4921e26f15f5df65bd6ddfe415f409743e7
Carl Rosenberger <carl.rosenberger@actian.com>
2021-05-21 16:44:25 +0200

    BAAS-587 - Further tests for NativeSQL queries

--- spec/nativesql1.spec.js
+++ spec/nativesql1.spec.js
@@ -5,23 +5,86 @@ if (typeof module !== 'undefined') {
 var emf, em;
 
 describe('More NativeSQL Tests', async function () {
+    it('Illegal select', async function () {
+        await setup();
+        await illegalSelect();
+    });
     it('Plain select', async function () {
-        await connect(false);
-        await produceMetaModel();
-        await connect(true);
-        await deleteStore();
-        await select();
+        await setup();
+        await storeOne();
+        await selectOne();
    });
+   it('Multiple rows', async function () {
+        await setup();
+        await storeThree();
+        await selectThree();
+    });
+    it('No row', async function () {
+        await setup();
+        await noRow();
+    });
+    it('Select column', async function () {
+        await setup();
+        await storeOne();
+        await selectColumn();
+    });
+    it('Just garbage', async function () {
+        await setup();
+        await garbage();
+    });
+    it('Unknown field', async function () {
+        await setup();
+        await unknownField();
+    });
+
+    
 });
 
-async function select(){
+async function illegalSelect(){
+    const response = await em.nativeQuery.execute('select * from NQOne where name = "one" ');
+    // TODO: Information about the failed query should be in the response.
+}
+
+async function selectOne(){
     const response = await em.nativeQuery.execute('select * from NQOne where name = \'one\' ');
     expect(response[1]["row"]["nqone:name"]).eql("one");
 }
 
-async function deleteStore(){
-    await deleteAllObjects();
-    const obj = await storeOneObject();
+async function selectThree(){
+    const response = await em.nativeQuery.execute('select * from NQOne');
+    var name = response[1]["row"]["nqone:name"];
+    expect(name == "1" || name == "2" || name == "3");
+    name = response[2]["row"]["nqone:name"];
+    expect(name == "1" || name == "2" || name == "3");
+    name = response[3]["row"]["nqone:name"];
+    expect(name == "1" || name == "2" || name == "3");
+}
+
+async function noRow(){
+    const response = await em.nativeQuery.execute('select * from NQOne where name = \'notexists\' ');
+    expect(response.size === 1);
+}
+
+async function selectColumn(){
+    const response = await em.nativeQuery.execute('select name from NQOne where name = \'one\' ');
+    expect(response[1]["row"]["nqone:name"]).eql("one");
+}
+
+async function garbage(){
+    const response = await em.nativeQuery.execute('gnuezelbruenft');
+    // TODO: Information about the failed query should be in the response.
+}
+
+async function unknownField(){
+    const response = await em.nativeQuery.execute('select notexists from NQOne');
+    // TODO: Information about the failed query should be in the response.
+}
+
+async function setup(){
+    await connect(false);
+    await produceMetaModel();
+    await connect(true);
+    await deleteAll();
 }
 
 async function connect(withSchema){
@@ -53,7 +116,7 @@ async function produceMetaModel(){
     await metamodel.save();
 }
 
-async function deleteAllObjects(){
+async function deleteAll(){
     await em.NQOne.find().resultList((result) => {
         result.forEach((obj) => {
             obj.delete();
@@ -61,9 +124,18 @@ async function deleteAllObjects(){
     });
 }
 
-async function storeOneObject(){
+async function storeOne(){
     var obj = new em.NQOne();
     obj.name = "one";
     await obj.save();
-    return obj;
 }
+
+
+async function storeThree(){
+    for(let i = 1; i <= 3; i++){
+        var obj = new em.NQOne();
+        obj.name = "" + i;
+        await obj.save();
+    }
+}
+
