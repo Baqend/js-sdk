4348fb61694f287f6a80d10ded5fc701f0bec8fc
fbeucklers <florian.buecklers@baqend.com>
2022-12-11 22:23:36 +0100

    feat(cli): Sync schema ACLs back by the deploy command

--- cli/schema.ts
+++ cli/schema.ts
@@ -12,35 +12,53 @@ type JsonMap = util.JsonMap;
 const SCHEMA_FILE_PATH = './baqend/schema/';
 
 export type SchemaArgs = {
-  command: 'upload' | 'download',
+  command: 'upload' | 'download' | 'push' | 'pull'
   force?: boolean,
 };
 
-export function uploadSchema(db: EntityManager, args: { force?: boolean } = {}): Promise<any> {
-  return readDir(SCHEMA_FILE_PATH)
-    .catch(() => {
-      throw new Error('Your schema folder baqend is empty, no schema changes were deployed.');
-    })
-    .then((fileNames) => Promise.all(
-      fileNames.map((fileName) => readFile(pathJoin(SCHEMA_FILE_PATH, fileName), 'utf-8').then((file) => JSON.parse(file) as JsonMap)),
-    ).then((schemas: JsonMap[]) => {
-      if (args.force) {
-        return helper.readInput('This will delete ALL your App data. Are you sure you want to continue? (yes/no)')
-          .then((answer: string) => {
-            if (answer.toLowerCase() === 'yes') {
-              schemas.forEach((schemaDescriptor: JsonMap) => {
-                console.log(`Replacing ${(schemaDescriptor.class as string).replace('/db/', '')} Schema`);
-              });
-              return db.send(new message.ReplaceAllSchemas(schemas)) as Promise<any>;
-            }
-            return Promise.resolve();
-          });
-      }
-      schemas.forEach((schemaDescriptor) => {
-        console.log(`Updating ${(schemaDescriptor.class as string).replace('/db/', '')} Schema`);
-      });
-      return db.send(new message.UpdateAllSchemas(schemas));
-    }));
+export async function uploadSchema(db: EntityManager, args: { force?: boolean } = {}): Promise<boolean> {
+  let fileNames;
+  try {
+    fileNames = await readDir(SCHEMA_FILE_PATH);
+  } catch (e) {
+    throw new Error('Your schema folder baqend is empty, no schema changes were deployed.');
+  }
+
+  const schemas: JsonMap[] = await Promise.all(
+    fileNames.map(async (fileName) => {
+      const file = await readFile(pathJoin(SCHEMA_FILE_PATH, fileName), 'utf-8');
+      return JSON.parse(file) as JsonMap;
+    }),
+  );
+
+  if (args.force) {
+    const answer: string = await helper.readInput('This will delete ALL your App data. Are you sure you want to continue? (yes/no)');
+    if (answer.toLowerCase() !== 'yes') {
+      return false;
+    }
+
+    schemas.forEach((schemaDescriptor: JsonMap) => {
+      console.log(`Replacing ${(schemaDescriptor.class as string).replace('/db/', '')} Schema`);
+    });
+    await db.send(new message.ReplaceAllSchemas(schemas));
+    return true;
+  }
+
+  const acls = schemas.map((schemaDescriptor) => {
+    console.log(`Updating ${(schemaDescriptor.class as string).replace('/db/', '')} Schema`);
+    return { operation: 'updateClassAcl', bucket: schemaDescriptor.class, acl: schemaDescriptor.acl };
+  });
+
+  try {
+    // We need to update acls separately, since they are not changed by an additive update
+    // Deploy schemas first to ensure all schemas exists
+    await db.send(new message.UpdateAllSchemas(schemas));
+    await db.send(new message.UpdateAllSchemas(acls));
+    return true;
+  } catch (e: any) {
+    console.error(`Schema update failed with error: ${e.message}`);
+    return false;
+  }
 }
 
 export function downloadSchema(db: EntityManager) {
@@ -63,6 +81,7 @@ export function schema(args: SchemaArgs & AccountArgs) {
   return account.login(args).then((db) => ensureDir(SCHEMA_FILE_PATH).then(() => {
     switch (args.command) {
       case 'upload':
+      case 'push':
         return uploadSchema(db, args).then((deployed) => {
           console.log('---------------------------------------');
           if (deployed) {
@@ -72,6 +91,7 @@ export function schema(args: SchemaArgs & AccountArgs) {
           }
         });
       case 'download':
+      case 'pull':
         return downloadSchema(db).then(() => {
           console.log('---------------------------------------');
           console.log('Your schema was successfully downloaded');
