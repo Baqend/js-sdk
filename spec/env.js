ee559652462015b17a87024c1940d17ed14bc219
Jannick Bock <jannick.bock@baqend.com>
2023-07-17 13:34:18 +0200

    fix: test_server var

--- spec/env.js
+++ spec/env.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var env = {
-  TEST_SERVER: 'https://local.baqend.com:8443/v1',
+  TEST_SERVER: process.env.TEST_SERVER || 'https://local.baqend.com:8443/v1',
 };
 
 if (typeof module !== 'undefined') {
