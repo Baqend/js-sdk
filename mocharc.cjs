ebc5d12efa39f52f9cfc340129efb3c5b452f7e9
Jannick Bock <jannick.bock@baqend.com>
2023-07-17 14:25:29 +0200

    add mochaFile result name

--- mocharc.cjs
+++ mocharc.cjs
@@ -3,8 +3,8 @@ const config = {
   quiet: false,
   package: './package.json',
   reporterOption: [
-    `mochaFile=./build/test-results/node-${process.version}.xml`,
-    'jenkinsMode=true',
+    `mochaFile=./build/test-results/node-result.xml`,
+    'jenkinsMode=false',
     `rootSuiteTitle="Node ${process.version} Test Suite"`,
   ],
   timeout: 4000,
