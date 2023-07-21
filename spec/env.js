4d85bebcc1e0baacbf7778e44feb0e7668e9ee04
Jannick Bock <jannick.bock@baqend.com>
2023-07-21 16:36:59 +0200

    fix: env

--- spec/env.js
+++ spec/env.js
@@ -1,12 +1,12 @@
 'use strict';
 
 var env = {
-    TEST_SERVER: 'https://local.baqend.com:8443/v1',
+    TEST_SERVER: 'https://local.baqend.com:8080/v1',
 };
 
 // set env to TEST_SERVER if we set a karma arg
 if (typeof __karma__ != 'undefined') {
-    env = {TEST_SERVER: __karma__.config.args || 'https://local.baqend.com:8443/v1'}
+    env = {TEST_SERVER: __karma__.config.args || 'https://local.baqend.com:8080/v1'}
 }
 
 if (typeof module !== 'undefined') {
